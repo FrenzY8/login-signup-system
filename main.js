dcae20b476ef19a8594494aed7ddcf7d70c12c91
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-11-08 14:18:42 +0700

    update simple

--- main.js
+++ main.js
@@ -5,71 +5,69 @@ var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-console.log("Write 'signup' for Countinue")
-rl.question("Answer : ", enterewoi => {
-  if(enterewoi == "signup") {
-  console.log("SIGN UP! (CREATE ACCOUNT)")
-    rl.question("enter your username : ", userdia => {
-      if(userdia == "") {
-        console.log("blank.")
+console.log("You must Sign Up first before use this tools")
+rl.question("Enter your names : ", namadia => {
+  if(namadia == "") {
+    console.log("At last you have a name.")
+  } else {
+    rl.question("Enter the password for your account : ", password => {
+      if(password == "") {
+        console.log("at last you have a password")
       } else {
-        rl.question("enter your password : ", passdia => {
-          if(passdia == "") {
-            console.log("blank.")
+        console.log(`We will write : 
+{
+  "users": "${namadia}",
+  "passwords": "${password}"
+}`)
+rl.question("This is ok? write n to cancel : ", nanyaok => {
+  if(nanyaok == "") {
+    console.log("please answer.")
+  } else {
+    if(nanyaok == "n") {
+      console.log("cancelled")
+    } else {
+      if(nanyaok == "N") {
+        console.log("cancelled")
+      } else {
+        fs.writeFileSync(
+          "db.json",
+          `{
+  "users": "${namadia}",
+  "passwords": "${password}"
+}`
+        );
+        console.log("Account Created")
+        const { users, passwords } = require('./db.json')
+        console.log("Login System!")
+        rl.question("enter your username : ", userlog => {
+          if(userlog == "") {
+            console.log("failed login.")
           } else {
-            fs.writeFileSync(
-              "data.json",
-              `{
-                "namaAkun": "${userdia}",
-                "passAkun": "${passdia}"
-              }`);
-              console.log("Account data has been created!")
-              console.log("LOGIN!")
-              rl.question("enter the username : ", loguser => {
-              if(loguser == userdia) {
+            if(userlog == users) {
               rl.question("enter the password : ", passlog => {
-              if(passlog == passdia) {
-              console.log("SUCCES LOGIN")
-              // YOUR CODE HERE!!!........ ANTIDDOS, DDOS TOOLS, ETC DAN LAIN LAIN.!!
-              
-              } else {
-              console.log("pass wrong.")
-              }
+                if(passlog == "") {
+                  console.log("failed log.")
+                } else {
+                  if(passlog == passwords) {
+                    console.log("SUCCES LOGIN")
+                    console.log("Login System by FrenzySG (FrenzY8)")
+                    // YOUR CODE HERE, ANTI DDOS, DDOS TOOLS, ETC.
+                    
+                  } else {
+                    console.log("wrong pass.")
+                  }
+                }
               })
-              } else {
-              console.log("wrong")
-              }
-              })
-            //console.log("data succes created.")
+            } else {
+              console.log("user wrong.")
+            }
           }
         })
+      }
+    }
+  }
+})
       }
     })
-  } else {
-  // BETA CODE, YOU WILL GET ERROR IF DO THIS!
-    if(enterewoi == "ERROR404040404040404044040404004040404040404040404") { // PLEASE DONT TRY THIS, CUZ THIS WILL BE ERROR!
-      const { namaAkun, passAkun } = require('./data.json')
-      const fs = require("fs");
-const path = "./data.json";
-if (fs.existsSync(path)) {
-  // path exists
-  rl.question("enter username : ", userlogin => {
-    if(userlogin == "") {
-      console.log("blank.")
-    } else {
-      if(userlogin == namaAkun) {
-        rl.question("enter password : ", passlog => {
-          if(passlog == passAkun) {
-            console.log("SUCCES LOGIN")
-            // YOUR CODE HERE!
-          }
-        })
-      }
-    }
-  })
-} else {
-  console.log("Please signup.");
-}
-    }
   }
 })
