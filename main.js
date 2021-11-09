c710ef5b95d2db0f543b5d039ec465d3fc68f916
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-11-09 08:32:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,19 +19,10 @@ rl.question("Select your method : ", methodDia => {
             if(passSign == "") {
               console.log("need pass.")
             } else {
-              fs.writeFileSync(
-                "data.json",
-                `{
-  "usernames": "${usersign}",
-  "passwords": "${passSign}"
-}`
-              );
-            }
-          })
-        }
-      }) // AUTO LOGIN IF DONE SIGN UP
-const { usernames, passwords } = require('./data.json');
-      console.log("LOGIN PAGE")
+              // auto login (BETA)
+              const { usernames, passwords } = require('./data.json');
+              console.log("AUTO-LOGIN PAGES!")
+              console.log("account created!")
       rl.question("enter your username : ", userlog2 => {
         if(userlog2 == "") {
           console.log("need username.")
@@ -45,7 +36,43 @@ const { usernames, passwords } = require('./data.json');
                   console.log("succes login!")
                   console.log("Lite Login signup by FrenzySG")
                   // your code here, anti ddos, ddos tools, ETC!
-                  
+
+                } else {
+                  console.log("wrong pass.. did you forget?")
+                }
+              }
+            })
+          } else {
+            console.log("wrong username.. did you forget?")
+          }
+        }
+      })
+              fs.writeFileSync(
+                "data.json",
+                `{
+  "usernames": "${usersign}",
+  "passwords": "${passSign}"
+}`
+              );
+            }
+          })
+        }
+      }) // AUTO LOGIN IF DONE SIGN UP
+const { usernames, passwords } = require('./data.json');
+      rl.question("enter your username : ", userlog2 => {
+        if(userlog2 == "") {
+          console.log("need username.")
+        } else {
+          if(userlog2 == usernames) {
+            rl.question("enter your password : ", passlog2 => {
+              if(passlog2 == "") {
+                console.log("need pass")
+              } else {
+                if(passlog2 == passwords) {
+                  console.log("succes login!")
+                  console.log("Lite Login signup by FrenzySG")
+                  // your code here, anti ddos, ddos tools, ETC!
+
                 } else {
                   console.log("wrong pass.. did you forget?")
                 }
