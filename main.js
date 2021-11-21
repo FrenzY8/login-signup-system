d8e5412c471946a35c8616938d2748ce1e14118c
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-11-21 04:15:00 +0000

    SecurityCode, Encrypted, Password, Encrypted

--- main.js
+++ main.js
@@ -1,143 +1,150 @@
+// USED FOR TOOLS
 const readline = require('readline');
 const fs = require('fs');
-const savedatabase = console.log;
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-console.log("Please write 'signup' to signup, 'login' to login")
-rl.question("Select your method : ", methodDia => {
-  if(methodDia == "") {
-    console.log("Select a method.")
-  } else {
-    if(methodDia == "signup") {
-    fs.writeFileSync(
-    "data.json",
-    `{
-       "nothing": "database isnt created."
-    }`);
-      rl.question("enter your username : ", usersign => {
-        if(usersign == "") {
-          console.log("need username")
-        } else {
-          rl.question("enter the password : ", passSign => {
-            if(passSign == "") {
-              console.log("need pass.")
-            } else {
-            fs.writeFileSync(
-                "config.json",
-                `{
-  "usernames": "${usersign}",
-  "passwords": "${passSign}"
-}`
-              );
-              // auto login (BETA)
-              const { usernames, passwords } = require('./config.json');
-              let userdb = usernames;
-              let passdb = passwords;
-              console.log("AUTO-LOGIN PAGES!")
-              console.log("account created!")
-      rl.question("enter your username : ", userlog2 => {
-        if(userlog2 == "") {
-          console.log("need username.")
-        } else {
-          if(userlog2 == userdb) {
-            rl.question("enter your password : ", passlog2 => {
-              if(passlog2 == "") {
-                console.log("need pass")
-              } else {
-                if(passlog2 == passdb) {
-                  console.log("succes login!")
-                  console.log("Lite Login signup by FrenzySG")
-                  // your code here, anti ddos, ddos tools, ETC!
-
-                } else {
-                  console.log("wrong pass.. did you forget?")
-                }
-              }
-            })
-          } else {
-            console.log("wrong username.. did you forget?")
-          }
-        }
-      })
-      fs.writeFileSync(
-                "data.json",
-                `{
-  "usernames": "${usersign}",
-  "passwords": "${passSign}"
-}`
-              );
-            }
-          })
-        }
-      }) // AUTO LOGIN IF DONE SIGN UP
-const { usernames, passwords } = require('./data.json');
-      rl.question("enter your username : ", userlog2 => {
-        if(userlog2 == "") {
-          console.log("need username.")
-        } else {
-          if(userlog2 == usernames) {
-            rl.question("enter your password : ", passlog2 => {
-              if(passlog2 == "") {
-                console.log("need pass")
-              } else {
-                if(passlog2 == passwords) {
-                  console.log("succes login!")
-                  console.log("Lite Login signup by FrenzySG")
-                  // your code here, anti ddos, ddos tools, ETC!
-
-                } else {
-                  console.log("wrong pass.. did you forget?")
-                }
-              }
-            })
-          } else {
-            console.log("wrong username.. did you forget?")
-          }
-        }
-      })
+    input: process.stdin,
+    output: process.stdout
+});
+// login / signup version 2
+console.log("Hi, write 'signup' to signup, 'login' to login");
+rl.question("Which one methode you want use : ", Methode => {
+    if(Methode == "") {
+        console.log("select signup or login pls")
     } else {
-    if(methodDia == "login") {
-      //const fs = require("fs")
-const path = "./config.json"
-try {
-  if (fs.existsSync(path)) {
-    const { usernames, passwords } = require('./data.json');
-    rl.question("Enter your username for login : ", userlog => {
-      if(userlog == "") {
-        console.log("failed login")
-      } else {
-        if(userlog == usernames) {
-          console.log("Usernames true.")
-          rl.question("Enter your password : ", passlog => {
-            if(passlog == "") {
-              console.log("cannot login, blank pass.")
-            } else {
-              if(passlog == passwords) {
-                console.log("LOGINNED! Tools succes accesed!")
-                console.log("Via : github.com/FrenzY8")
-                // YOUR CODE HERE
-                //ANTI DDOS, DDOS TOOLS, ETC!!
-              } else {
-                console.log("wrong pass.")
-              }
-            }
-          })
+        if(Methode == "signup") {
+            rl.question("Enter your name : ", namesign => {
+                if(namesign == "") {
+                    console.log("Must have names.")
+                } else {
+                    rl.question("enter your passwords : ", PassSign => {
+                        if(PassSign == "") {
+                            console.log("Need pass for your account")
+                        } else {
+                            console.log("Security Code will be used if you forget the password")
+                            rl.question("enter your security code : ", securityCode => {
+                                if(securityCode == "") {
+                                    console.log("you need security code")
+                                } else {
+// security will be encrypted!
+const securityEnc = securityCode;
+const buffSec = Buffer.from(securityEnc, 'utf-8');
+const SecurityEncrypted = buffSec.toString('base64');
+console.log(SecurityEncrypted);
+// PASSWORD WILL BE ENCRYPTED!
+const str = PassSign;
+const buff = Buffer.from(str, 'utf-8');
+const base64 = buff.toString('base64');
+console.log(base64);
+fs.writeFileSync(
+"db.json",
+`{
+    "name": "${namesign}",
+    "passwordAcc": "${base64}",
+    "securityCodes": "${SecurityEncrypted}"
+}`)
+console.clear();
+                                    console.log("Account Created!")
+                                    console.log("LOGIN-PAGES!")
+                                    console.log("[AUTO-LOGIN]")
+                                    const { name, passwordAcc, securityCodes } = require('./db.json');
+                                    rl.question("Enter Your Name : ", NameLog => {
+                                        if(NameLog == "") {
+                                            console.log("to login, you need a name.")
+                                        } else {
+                                            if(NameLog == name) {
+                                                console.log("Usernames true.")
+                                                console.log("write 'forget' if you forget your password")
+                                                rl.question("Enter Your Password : ", passlog => {
+                                                    // protection
+                                                    if(passlog == "") {
+                                                        console.log("Need password for login!")
+                                                    } else {
+                                                        if(passlog == "forget") {
+                                                            console.log("Forget Passwords?")
+                                                            rl.question("Enter your security codes : ", forgetsecurity => {
+                                                                if(forgetsecurity == "") {
+                                                                    console.log("sorry wrong security code.")
+                                                                } else {
+                                                                    if(forgetsecurity == securityCodes) {
+const base64 = passwordAcc;
+const buff = Buffer.from(base64, 'base64');
+const str = buff.toString('utf-8');
+console.log(str);
+                                                                        console.log("Password Showen!")
+                                                                        console.log(`Your Password : ${str}`)
+                                                                        console.log("dont forget it.")
+                                                                    } else {
+                                                                        console.log("wrong security code")
+                                                                    }
+                                                                }
+                                                            })
+                                                        } else {
+                                                            if(passlog == passwordAcc) {
+                                                                console.log("Succes Login!")
+                                                                console.log("this code ended.")
+                                                                // YOUR CODE HERE
+                                                              
+                                                            } else {
+                                                                console.log("wrong password.")
+                                                            }
+                                                        }
+                                                    }
+                                                })
+                                            } else {
+                                                console.log("your username wrong. did you forget it?")
+                                            }
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+            })
         } else {
-          console.log("username wrong.")
+            if(Methode == "login") {
+const logpath = "./db.json"
+try {
+  if (fs.existsSync(logpath)) {
+      console.log("Welcome back!")
+      const { name, passwordAcc, securityCodes } = require('./db.json');
+    rl.question("Enter Your Name : ", userlog => {
+        if(userlog == "") {
+            console.log("Hmmm.. thats not a username.")
+        } else {
+            if(userlog == name) {
+                console.log("nice")
+                rl.question("Enter the Passwords : ", passlog => {
+                    //protection
+                    if(passlog == "") {
+                        console.log("need pass :0")
+                    } else {
+                        if(passlog == passwordAcc) {
+                            console.log("Succes Login!")
+                            console.log("this code ended.")
+                            // YOUR CODE HERE !!
+
+                        } else {
+                            console.log("password wrong")
+                        }
+                    }
+                })
+            } else {
+                console.log("wrong usernames.")
+            }
         }
-      }
     })
   } else {
-    console.log("Please signup, we did not found your data...")
+    console.log("Looks like you're new member, please signup!")
   }
 } catch(err) {
   console.error(err)
 }
-    } else {
-      console.log("Unknown Command, the currently commands are 'signup' , 'login'")
+            } else {
+                console.log("Unknown Commands :(")
+            }
+        }
     }
-    }
-  }
 })
+
+// lol
